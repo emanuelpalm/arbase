c81b210b44ad1d75d10ae62cb52fd5e23a4b1da6
Emanuel Palm <emanuelpalm@gmail.com>
2019-09-21 15:06:43 +0200

    Add safe int cast functions

--- arev_base/src/int.c
+++ arev_base/src/int.c
@@ -50,3 +50,73 @@ GEN_SUB(int_SubU8, uint8_t)
 GEN_SUB(int_SubU16, uint16_t)
 GEN_SUB(int_SubU32, uint32_t)
 GEN_SUB(int_SubU64, uint64_t)
+
+#define GEN_CAST_BOTH(NAME, TYPE_IN, TYPE_OUT, TYPE_OUT_MIN, TYPE_OUT_MAX) \
+    inline                                                                 \
+    err_t NAME(TYPE_IN in, TYPE_OUT *out) {                                \
+        if (in < TYPE_OUT_MIN) {                                           \
+            return ERROR_INT_UNDERFLOW;                                    \
+        }                                                                  \
+        if (in > TYPE_OUT_MAX) {                                           \
+            return ERROR_INT_OVERFLOW;                                     \
+        }                                                                  \
+        *out = (TYPE_OUT) in;                                              \
+        return ERROR_NONE;                                                 \
+    }
+#define GEN_CAST_DOWN(NAME, TYPE_IN, TYPE_OUT, TYPE_OUT_MAX) \
+    inline                                                   \
+    err_t NAME(TYPE_IN in, TYPE_OUT *out) {                  \
+        if (in > TYPE_OUT_MAX) {                             \
+            return ERROR_INT_OVERFLOW;                       \
+        }                                                    \
+        *out = (TYPE_OUT) in;                                \
+        return ERROR_NONE;                                   \
+    }
+#define GEN_CAST_TRIM(NAME, TYPE_IN, TYPE_OUT) \
+    inline                                     \
+    err_t NAME(TYPE_IN in, TYPE_OUT *out) {    \
+        if (in < 0) {                          \
+            return ERROR_INT_UNDERFLOW;        \
+        }                                      \
+        *out = (TYPE_OUT) in;                  \
+        return ERROR_NONE;                     \
+    }
+
+GEN_CAST_TRIM(int_CastI8ToU8, int8_t, uint8_t)
+GEN_CAST_TRIM(int_CastI8ToU16, int8_t, uint16_t)
+GEN_CAST_TRIM(int_CastI8ToU32, int8_t, uint32_t)
+GEN_CAST_TRIM(int_CastI8ToU64, int8_t, uint64_t)
+GEN_CAST_BOTH(int_CastI16ToI8, int16_t, int8_t, INT8_MIN, INT8_MAX)
+GEN_CAST_TRIM(int_CastI16ToU8, int16_t, uint8_t)
+GEN_CAST_TRIM(int_CastI16ToU16, int16_t, uint16_t)
+GEN_CAST_TRIM(int_CastI16ToU32, int16_t, uint32_t)
+GEN_CAST_TRIM(int_CastI16ToU64, int16_t, uint64_t)
+GEN_CAST_BOTH(int_CastI32ToI8, int32_t, int8_t, INT8_MIN, INT8_MAX)
+GEN_CAST_BOTH(int_CastI32ToI16, int32_t, int16_t, INT16_MIN, INT16_MAX)
+GEN_CAST_TRIM(int_CastI32ToU8, int32_t, uint8_t)
+GEN_CAST_TRIM(int_CastI32ToU16, int32_t, uint16_t)
+GEN_CAST_TRIM(int_CastI32ToU32, int32_t, uint32_t)
+GEN_CAST_TRIM(int_CastI32ToU64, int32_t, uint64_t)
+GEN_CAST_BOTH(int_CastI64ToI8, int64_t, int8_t, INT8_MIN, INT8_MAX)
+GEN_CAST_BOTH(int_CastI64ToI16, int64_t, int16_t, INT16_MIN, INT16_MAX)
+GEN_CAST_BOTH(int_CastI64ToI32, int64_t, int32_t, INT32_MIN, INT32_MAX)
+GEN_CAST_TRIM(int_CastI64ToU8, int64_t, uint8_t)
+GEN_CAST_TRIM(int_CastI64ToU16, int64_t, uint16_t)
+GEN_CAST_TRIM(int_CastI64ToU32, int64_t, uint32_t)
+GEN_CAST_TRIM(int_CastI64ToU64, int64_t, uint64_t)
+GEN_CAST_DOWN(int_CastU8ToI8, uint8_t, int8_t, INT8_MAX)
+GEN_CAST_DOWN(int_CastU16ToI8, uint16_t, int8_t, INT8_MAX)
+GEN_CAST_DOWN(int_CastU16ToI16, uint16_t, int16_t, INT16_MAX)
+GEN_CAST_DOWN(int_CastU16ToU8, uint16_t, uint8_t, UINT8_MAX)
+GEN_CAST_DOWN(int_CastU32ToI8, uint32_t, int8_t, INT8_MAX)
+GEN_CAST_DOWN(int_CastU32ToI16, uint32_t, int16_t, INT16_MAX)
+GEN_CAST_DOWN(int_CastU32ToI32, uint32_t, int32_t, INT32_MAX)
+GEN_CAST_DOWN(int_CastU32ToU8, uint32_t, uint8_t, UINT8_MAX)
+GEN_CAST_DOWN(int_CastU32ToU16, uint32_t, uint16_t, UINT16_MAX)
+GEN_CAST_DOWN(int_CastU64ToI8, uint64_t, int8_t, INT8_MAX)
+GEN_CAST_DOWN(int_CastU64ToI16, uint64_t, int16_t, INT16_MAX)
+GEN_CAST_DOWN(int_CastU64ToI32, uint64_t, int32_t, INT32_MAX)
+GEN_CAST_DOWN(int_CastU64ToI64, uint64_t, int64_t, INT64_MAX)
+GEN_CAST_DOWN(int_CastU64ToU8, uint64_t, uint8_t, UINT8_MAX)
+GEN_CAST_DOWN(int_CastU64ToU16, uint64_t, uint16_t, UINT16_MAX)
+GEN_CAST_DOWN(int_CastU64ToU32, uint64_t, uint32_t, UINT32_MAX)
