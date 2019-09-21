78da6312eab373c402a2a569ea56487e3d615cbb
Emanuel Palm <emanuelpalm@gmail.com>
2019-09-21 21:45:07 +0200

    Add more safe integer functions

--- arev_base/src/int.c
+++ arev_base/src/int.c
@@ -1,30 +1,45 @@
 #include "int.h"
 
 #ifdef AREV_GCC
-#define GEN_ADD(NAME, TYPE)                      \
-    inline                                       \
-    err_t NAME(TYPE a, TYPE b, TYPE *out) {      \
-        return __builtin_add_overflow(a, b, out) \
-            ? ERROR_INT_OVERFLOW                 \
-            : ERROR_NONE;                        \
-    }
-#define GEN_MUL(NAME, TYPE)                      \
-    inline                                       \
-    err_t NAME(TYPE a, TYPE b, TYPE *out) {      \
-        return __builtin_mul_overflow(a, b, out) \
-            ? ERROR_INT_OVERFLOW                 \
-            : ERROR_NONE;                        \
-    }
-#define GEN_SUB(NAME, TYPE)                      \
-    inline                                       \
-    err_t NAME(TYPE a, TYPE b, TYPE *out) {      \
-        return __builtin_sub_overflow(a, b, out) \
-            ? ERROR_INT_UNDERFLOW                \
-            : ERROR_NONE;                        \
-    }
+
+#define GEN_ADD(NAME, TYPE)                  \
+inline                                       \
+err_t NAME(TYPE a, TYPE b, TYPE *out) {      \
+    return __builtin_add_overflow(a, b, out) \
+        ? ERROR_INT_OVERFLOW                 \
+        : ERROR_NONE;                        \
+}
+
+#define GEN_MUL(NAME, TYPE)                  \
+inline                                       \
+err_t NAME(TYPE a, TYPE b, TYPE *out) {      \
+    return __builtin_mul_overflow(a, b, out) \
+        ? ERROR_INT_OVERFLOW                 \
+        : ERROR_NONE;                        \
+}
+
+#define GEN_SUB(NAME, TYPE)                  \
+inline                                       \
+err_t NAME(TYPE a, TYPE b, TYPE *out) {      \
+    return __builtin_sub_overflow(a, b, out) \
+        ? ERROR_INT_UNDERFLOW                \
+        : ERROR_NONE;                        \
+}
+
 #else
 #error No known checked integer operation intrinsics are available.
 #endif
+
+#define GEN_DIV(NAME, TYPE)                  \
+inline                                       \
+err_t NAME(TYPE a, TYPE b, TYPE *out) {      \
+    if (b == 0) {                            \
+        return ERROR_INT_DIVZERO;            \
+    }                                        \
+    *out = a / b;                            \
+    return ERROR_NONE;                       \
+}
+
 
 GEN_ADD(int_AddI8, int8_t)
 GEN_ADD(int_AddI16, int16_t)
@@ -34,6 +49,18 @@ GEN_ADD(int_AddU8, uint8_t)
 GEN_ADD(int_AddU16, uint16_t)
 GEN_ADD(int_AddU32, uint32_t)
 GEN_ADD(int_AddU64, uint64_t)
+GEN_ADD(int_AddSize, size_t)
+
+GEN_DIV(int_DivI8, int8_t)
+GEN_DIV(int_DivI16, int16_t)
+GEN_DIV(int_DivI32, int32_t)
+GEN_DIV(int_DivI64, int64_t)
+GEN_DIV(int_DivU8, uint8_t)
+GEN_DIV(int_DivU16, uint16_t)
+GEN_DIV(int_DivU32, uint32_t)
+GEN_DIV(int_DivU64, uint64_t)
+GEN_DIV(int_DivSize, size_t)
+
 GEN_MUL(int_MulI8, int8_t)
 GEN_MUL(int_MulI16, int16_t)
 GEN_MUL(int_MulI32, int32_t)
@@ -42,6 +69,8 @@ GEN_MUL(int_MulU8, uint8_t)
 GEN_MUL(int_MulU16, uint16_t)
 GEN_MUL(int_MulU32, uint32_t)
 GEN_MUL(int_MulU64, uint64_t)
+GEN_ADD(int_MulSize, size_t)
+
 GEN_SUB(int_SubI8, int8_t)
 GEN_SUB(int_SubI16, int16_t)
 GEN_SUB(int_SubI32, int32_t)
@@ -50,73 +79,123 @@ GEN_SUB(int_SubU8, uint8_t)
 GEN_SUB(int_SubU16, uint16_t)
 GEN_SUB(int_SubU32, uint32_t)
 GEN_SUB(int_SubU64, uint64_t)
+GEN_ADD(int_SubSize, size_t)
 
-#define GEN_CAST_BOTH(NAME, TYPE_IN, TYPE_OUT, TYPE_OUT_MIN, TYPE_OUT_MAX) \
-    inline                                                                 \
-    err_t NAME(TYPE_IN in, TYPE_OUT *out) {                                \
-        if (in < TYPE_OUT_MIN) {                                           \
-            return ERROR_INT_UNDERFLOW;                                    \
-        }                                                                  \
-        if (in > TYPE_OUT_MAX) {                                           \
-            return ERROR_INT_OVERFLOW;                                     \
-        }                                                                  \
-        *out = (TYPE_OUT) in;                                              \
-        return ERROR_NONE;                                                 \
-    }
-#define GEN_CAST_DOWN(NAME, TYPE_IN, TYPE_OUT, TYPE_OUT_MAX) \
-    inline                                                   \
-    err_t NAME(TYPE_IN in, TYPE_OUT *out) {                  \
-        if (in > TYPE_OUT_MAX) {                             \
-            return ERROR_INT_OVERFLOW;                       \
-        }                                                    \
-        *out = (TYPE_OUT) in;                                \
-        return ERROR_NONE;                                   \
-    }
-#define GEN_CAST_TRIM(NAME, TYPE_IN, TYPE_OUT) \
-    inline                                     \
-    err_t NAME(TYPE_IN in, TYPE_OUT *out) {    \
-        if (in < 0) {                          \
-            return ERROR_INT_UNDERFLOW;        \
-        }                                      \
-        *out = (TYPE_OUT) in;                  \
-        return ERROR_NONE;                     \
-    }
+#define GEN_CAST_IX_TO_IX(NAME, TYPE_IN, TYPE_OUT, TYPE_OUT_MIN, TYPE_OUT_MAX) \
+inline                                                                         \
+err_t NAME(TYPE_IN in, TYPE_OUT *out) {                                        \
+    if (in < TYPE_OUT_MIN) {                                                   \
+        return ERROR_INT_UNDERFLOW;                                            \
+    }                                                                          \
+    if (in > TYPE_OUT_MAX) {                                                   \
+        return ERROR_INT_OVERFLOW;                                             \
+    }                                                                          \
+    *out = (TYPE_OUT) in;                                                      \
+    return ERROR_NONE;                                                         \
+}
 
-GEN_CAST_TRIM(int_CastI8ToU8, int8_t, uint8_t)
-GEN_CAST_TRIM(int_CastI8ToU16, int8_t, uint16_t)
-GEN_CAST_TRIM(int_CastI8ToU32, int8_t, uint32_t)
-GEN_CAST_TRIM(int_CastI8ToU64, int8_t, uint64_t)
-GEN_CAST_BOTH(int_CastI16ToI8, int16_t, int8_t, INT8_MIN, INT8_MAX)
-GEN_CAST_TRIM(int_CastI16ToU8, int16_t, uint8_t)
-GEN_CAST_TRIM(int_CastI16ToU16, int16_t, uint16_t)
-GEN_CAST_TRIM(int_CastI16ToU32, int16_t, uint32_t)
-GEN_CAST_TRIM(int_CastI16ToU64, int16_t, uint64_t)
-GEN_CAST_BOTH(int_CastI32ToI8, int32_t, int8_t, INT8_MIN, INT8_MAX)
-GEN_CAST_BOTH(int_CastI32ToI16, int32_t, int16_t, INT16_MIN, INT16_MAX)
-GEN_CAST_TRIM(int_CastI32ToU8, int32_t, uint8_t)
-GEN_CAST_TRIM(int_CastI32ToU16, int32_t, uint16_t)
-GEN_CAST_TRIM(int_CastI32ToU32, int32_t, uint32_t)
-GEN_CAST_TRIM(int_CastI32ToU64, int32_t, uint64_t)
-GEN_CAST_BOTH(int_CastI64ToI8, int64_t, int8_t, INT8_MIN, INT8_MAX)
-GEN_CAST_BOTH(int_CastI64ToI16, int64_t, int16_t, INT16_MIN, INT16_MAX)
-GEN_CAST_BOTH(int_CastI64ToI32, int64_t, int32_t, INT32_MIN, INT32_MAX)
-GEN_CAST_TRIM(int_CastI64ToU8, int64_t, uint8_t)
-GEN_CAST_TRIM(int_CastI64ToU16, int64_t, uint16_t)
-GEN_CAST_TRIM(int_CastI64ToU32, int64_t, uint32_t)
-GEN_CAST_TRIM(int_CastI64ToU64, int64_t, uint64_t)
-GEN_CAST_DOWN(int_CastU8ToI8, uint8_t, int8_t, INT8_MAX)
-GEN_CAST_DOWN(int_CastU16ToI8, uint16_t, int8_t, INT8_MAX)
-GEN_CAST_DOWN(int_CastU16ToI16, uint16_t, int16_t, INT16_MAX)
-GEN_CAST_DOWN(int_CastU16ToU8, uint16_t, uint8_t, UINT8_MAX)
-GEN_CAST_DOWN(int_CastU32ToI8, uint32_t, int8_t, INT8_MAX)
-GEN_CAST_DOWN(int_CastU32ToI16, uint32_t, int16_t, INT16_MAX)
-GEN_CAST_DOWN(int_CastU32ToI32, uint32_t, int32_t, INT32_MAX)
-GEN_CAST_DOWN(int_CastU32ToU8, uint32_t, uint8_t, UINT8_MAX)
-GEN_CAST_DOWN(int_CastU32ToU16, uint32_t, uint16_t, UINT16_MAX)
-GEN_CAST_DOWN(int_CastU64ToI8, uint64_t, int8_t, INT8_MAX)
-GEN_CAST_DOWN(int_CastU64ToI16, uint64_t, int16_t, INT16_MAX)
-GEN_CAST_DOWN(int_CastU64ToI32, uint64_t, int32_t, INT32_MAX)
-GEN_CAST_DOWN(int_CastU64ToI64, uint64_t, int64_t, INT64_MAX)
-GEN_CAST_DOWN(int_CastU64ToU8, uint64_t, uint8_t, UINT8_MAX)
-GEN_CAST_DOWN(int_CastU64ToU16, uint64_t, uint16_t, UINT16_MAX)
-GEN_CAST_DOWN(int_CastU64ToU32, uint64_t, uint32_t, UINT32_MAX)
+#define GEN_CAST_UX_TO_XX(NAME, TYPE_IN, TYPE_OUT, TYPE_OUT_MAX) \
+inline                                                           \
+err_t NAME(TYPE_IN in, TYPE_OUT *out) {                          \
+    if (in > TYPE_OUT_MAX) {                                     \
+        return ERROR_INT_OVERFLOW;                               \
+    }                                                            \
+    *out = (TYPE_OUT) in;                                        \
+    return ERROR_NONE;                                           \
+}
+
+#define GEN_CAST_IX_TO_UX(NAME, TYPE_IN, TYPE_OUT) \
+inline                                             \
+err_t NAME(TYPE_IN in, TYPE_OUT *out) {            \
+    if (in < 0) {                                  \
+        return ERROR_INT_UNDERFLOW;                \
+    }                                              \
+    *out = (TYPE_OUT) in;                          \
+    return ERROR_NONE;                             \
+}
+
+#define GEN_CAST_UNCHECKED(NAME, TYPE_IN, TYPE_OUT) \
+inline                                              \
+err_t NAME(TYPE_IN in, TYPE_OUT *out) {             \
+    *out = (TYPE_OUT) in;                           \
+    return ERROR_NONE;                              \
+}
+
+GEN_CAST_IX_TO_UX(int_CastI8ToU8, int8_t, uint8_t)
+GEN_CAST_IX_TO_UX(int_CastI8ToU16, int8_t, uint16_t)
+GEN_CAST_IX_TO_UX(int_CastI8ToU32, int8_t, uint32_t)
+GEN_CAST_IX_TO_UX(int_CastI8ToU64, int8_t, uint64_t)
+GEN_CAST_IX_TO_UX(int_CastI8ToSize, int8_t, size_t)
+
+GEN_CAST_IX_TO_IX(int_CastI16ToI8, int16_t, int8_t, INT8_MIN, INT8_MAX)
+GEN_CAST_IX_TO_UX(int_CastI16ToU8, int16_t, uint8_t)
+GEN_CAST_IX_TO_UX(int_CastI16ToU16, int16_t, uint16_t)
+GEN_CAST_IX_TO_UX(int_CastI16ToU32, int16_t, uint32_t)
+GEN_CAST_IX_TO_UX(int_CastI16ToU64, int16_t, uint64_t)
+GEN_CAST_IX_TO_UX(int_CastI16ToSize, int16_t, size_t)
+
+GEN_CAST_IX_TO_IX(int_CastI32ToI8, int32_t, int8_t, INT8_MIN, INT8_MAX)
+GEN_CAST_IX_TO_IX(int_CastI32ToI16, int32_t, int16_t, INT16_MIN, INT16_MAX)
+GEN_CAST_IX_TO_UX(int_CastI32ToU8, int32_t, uint8_t)
+GEN_CAST_IX_TO_UX(int_CastI32ToU16, int32_t, uint16_t)
+GEN_CAST_IX_TO_UX(int_CastI32ToU32, int32_t, uint32_t)
+GEN_CAST_IX_TO_UX(int_CastI32ToU64, int32_t, uint64_t)
+GEN_CAST_IX_TO_UX(int_CastI32ToSize, int32_t, size_t)
+
+GEN_CAST_IX_TO_IX(int_CastI64ToI8, int64_t, int8_t, INT8_MIN, INT8_MAX)
+GEN_CAST_IX_TO_IX(int_CastI64ToI16, int64_t, int16_t, INT16_MIN, INT16_MAX)
+GEN_CAST_IX_TO_IX(int_CastI64ToI32, int64_t, int32_t, INT32_MIN, INT32_MAX)
+GEN_CAST_IX_TO_UX(int_CastI64ToU8, int64_t, uint8_t)
+GEN_CAST_IX_TO_UX(int_CastI64ToU16, int64_t, uint16_t)
+GEN_CAST_IX_TO_UX(int_CastI64ToU32, int64_t, uint32_t)
+GEN_CAST_IX_TO_UX(int_CastI64ToU64, int64_t, uint64_t)
+GEN_CAST_IX_TO_UX(int_CastI64ToSize, int64_t, size_t)
+
+GEN_CAST_UX_TO_XX(int_CastU8ToI8, uint8_t, int8_t, INT8_MAX)
+#if UINT8_MAX > SIZE_MAX
+GEN_CAST_UX_TO_XX(int_CastU8ToSize, uint8_t, size_t , SIZE_MAX)
+#else
+GEN_CAST_UNCHECKED(int_CastU8ToSize, uint8_t, size_t)
+#endif
+
+GEN_CAST_UX_TO_XX(int_CastU16ToI8, uint16_t, int8_t, INT8_MAX)
+GEN_CAST_UX_TO_XX(int_CastU16ToI16, uint16_t, int16_t, INT16_MAX)
+GEN_CAST_UX_TO_XX(int_CastU16ToU8, uint16_t, uint8_t, UINT8_MAX)
+#if UINT16_MAX > SIZE_MAX
+GEN_CAST_UX_TO_XX(int_CastU16ToSize, uint16_t, size_t, SIZE_MAX)
+#else
+GEN_CAST_UNCHECKED(int_CastU16ToSize, uint16_t, size_t)
+#endif
+
+GEN_CAST_UX_TO_XX(int_CastU32ToI8, uint32_t, int8_t, INT8_MAX)
+GEN_CAST_UX_TO_XX(int_CastU32ToI16, uint32_t, int16_t, INT16_MAX)
+GEN_CAST_UX_TO_XX(int_CastU32ToI32, uint32_t, int32_t, INT32_MAX)
+GEN_CAST_UX_TO_XX(int_CastU32ToU8, uint32_t, uint8_t, UINT8_MAX)
+GEN_CAST_UX_TO_XX(int_CastU32ToU16, uint32_t, uint16_t, UINT16_MAX)
+#if UINT32_MAX > SIZE_MAX
+GEN_CAST_UX_TO_XX(int_CastU32ToSize, uint32_t, size_t, SIZE_MAX)
+#else
+GEN_CAST_UNCHECKED(int_CastU32ToSize, uint32_t, size_t)
+#endif
+
+GEN_CAST_UX_TO_XX(int_CastU64ToI8, uint64_t, int8_t, INT8_MAX)
+GEN_CAST_UX_TO_XX(int_CastU64ToI16, uint64_t, int16_t, INT16_MAX)
+GEN_CAST_UX_TO_XX(int_CastU64ToI32, uint64_t, int32_t, INT32_MAX)
+GEN_CAST_UX_TO_XX(int_CastU64ToI64, uint64_t, int64_t, INT64_MAX)
+GEN_CAST_UX_TO_XX(int_CastU64ToU8, uint64_t, uint8_t, UINT8_MAX)
+GEN_CAST_UX_TO_XX(int_CastU64ToU16, uint64_t, uint16_t, UINT16_MAX)
+GEN_CAST_UX_TO_XX(int_CastU64ToU32, uint64_t, uint32_t, UINT32_MAX)
+#if UINT64_MAX > SIZE_MAX
+GEN_CAST_UX_TO_XX(int_CastU64ToSize, uint64_t, size_t, SIZE_MAX)
+#else
+GEN_CAST_UNCHECKED(int_CastU64ToSize, uint64_t, size_t)
+#endif
+
+GEN_CAST_UX_TO_XX(int_CastSizeToI8, size_t, int8_t, INT8_MAX)
+GEN_CAST_UX_TO_XX(int_CastSizeToI16, size_t, int16_t, INT16_MAX)
+GEN_CAST_UX_TO_XX(int_CastSizeToI32, size_t, int32_t, INT32_MAX)
+GEN_CAST_UX_TO_XX(int_CastSizeToI64, size_t, int64_t, INT64_MAX)
+GEN_CAST_UX_TO_XX(int_CastSizeToU8, size_t, uint8_t, UINT8_MAX)
+GEN_CAST_UX_TO_XX(int_CastSizeToU16, size_t, uint16_t, UINT16_MAX)
+GEN_CAST_UX_TO_XX(int_CastSizeToU32, size_t, uint32_t, UINT32_MAX)
+GEN_CAST_UX_TO_XX(int_CastSizeToU64, size_t, uint64_t, UINT64_MAX)
--- arev_base/src/int.h
+++ arev_base/src/int.h
@@ -3,11 +3,14 @@
 
 /// Safe integer operations.
 ///
-/// By safe, it is implied that use of these functions must never cause
-/// undefined behavior.
+/// More specifically, all functions defined in this file are guaranteed to
+/// never cause undefined behavior. That being said, no guarantees are given
+/// about the state of out variables if these functions return any other result
+/// than ERROR_NONE.
 ///
 /// @file
 
+#include <stddef.h>
 #include <stdint.h>
 #include "err.h"
 
@@ -19,6 +22,18 @@ err_t int_AddU8(uint8_t a, uint8_t b, uint8_t *out);
 err_t int_AddU16(uint16_t a, uint16_t b, uint16_t *out);
 err_t int_AddU32(uint32_t a, uint32_t b, uint32_t *out);
 err_t int_AddU64(uint64_t a, uint64_t b, uint64_t *out);
+err_t int_AddSize(size_t a, size_t b, size_t *out);
+
+err_t int_DivI8(int8_t a, int8_t b, int8_t *out);
+err_t int_DivI16(int16_t a, int16_t b, int16_t *out);
+err_t int_DivI32(int32_t a, int32_t b, int32_t *out);
+err_t int_DivI64(int64_t a, int64_t b, int64_t *out);
+err_t int_DivU8(uint8_t a, uint8_t b, uint8_t *out);
+err_t int_DivU16(uint16_t a, uint16_t b, uint16_t *out);
+err_t int_DivU32(uint32_t a, uint32_t b, uint32_t *out);
+err_t int_DivU64(uint64_t a, uint64_t b, uint64_t *out);
+err_t int_DivSize(size_t a, size_t b, size_t *out);
+
 err_t int_MulI8(int8_t a, int8_t b, int8_t *out);
 err_t int_MulI16(int16_t a, int16_t b, int16_t *out);
 err_t int_MulI32(int32_t a, int32_t b, int32_t *out);
@@ -27,6 +42,8 @@ err_t int_MulU8(uint8_t a, uint8_t b, uint8_t *out);
 err_t int_MulU16(uint16_t a, uint16_t b, uint16_t *out);
 err_t int_MulU32(uint32_t a, uint32_t b, uint32_t *out);
 err_t int_MulU64(uint64_t a, uint64_t b, uint64_t *out);
+err_t int_MulSize(size_t a, size_t b, size_t *out);
+
 err_t int_SubI8(int8_t a, int8_t b, int8_t *out);
 err_t int_SubI16(int16_t a, int16_t b, int16_t *out);
 err_t int_SubI32(int32_t a, int32_t b, int32_t *out);
@@ -35,22 +52,29 @@ err_t int_SubU8(uint8_t a, uint8_t b, uint8_t *out);
 err_t int_SubU16(uint16_t a, uint16_t b, uint16_t *out);
 err_t int_SubU32(uint32_t a, uint32_t b, uint32_t *out);
 err_t int_SubU64(uint64_t a, uint64_t b, uint64_t *out);
+err_t int_SubSize(size_t a, size_t b, size_t *out);
 
 err_t int_CastI8ToU8(int8_t in, uint8_t *out);
 err_t int_CastI8ToU16(int8_t in, uint16_t *out);
 err_t int_CastI8ToU32(int8_t in, uint32_t *out);
 err_t int_CastI8ToU64(int8_t in, uint64_t *out);
+err_t int_CastI8ToSize(int8_t in, size_t *out);
+
 err_t int_CastI16ToI8(int16_t in, int8_t *out);
 err_t int_CastI16ToU8(int16_t in, uint8_t *out);
 err_t int_CastI16ToU16(int16_t in, uint16_t *out);
 err_t int_CastI16ToU32(int16_t in, uint32_t *out);
 err_t int_CastI16ToU64(int16_t in, uint64_t *out);
+err_t int_CastI16ToSize(int16_t in, size_t *out);
+
 err_t int_CastI32ToI8(int32_t in, int8_t *out);
 err_t int_CastI32ToI16(int32_t in, int16_t *out);
 err_t int_CastI32ToU8(int32_t in, uint8_t *out);
 err_t int_CastI32ToU16(int32_t in, uint16_t *out);
 err_t int_CastI32ToU32(int32_t in, uint32_t *out);
 err_t int_CastI32ToU64(int32_t in, uint64_t *out);
+err_t int_CastI32ToSize(int32_t in, size_t *out);
+
 err_t int_CastI64ToI8(int64_t in, int8_t *out);
 err_t int_CastI64ToI16(int64_t in, int16_t *out);
 err_t int_CastI64ToI32(int64_t in, int32_t *out);
@@ -58,15 +82,23 @@ err_t int_CastI64ToU8(int64_t in, uint8_t *out);
 err_t int_CastI64ToU16(int64_t in, uint16_t *out);
 err_t int_CastI64ToU32(int64_t in, uint32_t *out);
 err_t int_CastI64ToU64(int64_t in, uint64_t *out);
+err_t int_CastI64ToSize(int64_t in, size_t *out);
+
 err_t int_CastU8ToI8(uint8_t in, int8_t *out);
+err_t int_CastU8ToSize(uint8_t in, size_t *out);
+
 err_t int_CastU16ToI8(uint16_t in, int8_t *out);
 err_t int_CastU16ToI16(uint16_t in, int16_t *out);
 err_t int_CastU16ToU8(uint16_t in, uint8_t *out);
+err_t int_CastU16ToSize(uint16_t in, size_t *out);
+
 err_t int_CastU32ToI8(uint32_t in, int8_t *out);
 err_t int_CastU32ToI16(uint32_t in, int16_t *out);
 err_t int_CastU32ToI32(uint32_t in, int32_t *out);
 err_t int_CastU32ToU8(uint32_t in, uint8_t *out);
 err_t int_CastU32ToU16(uint32_t in, uint16_t *out);
+err_t int_CastU32ToSize(uint32_t in, size_t *out);
+
 err_t int_CastU64ToI8(uint64_t in, int8_t *out);
 err_t int_CastU64ToI16(uint64_t in, int16_t *out);
 err_t int_CastU64ToI32(uint64_t in, int32_t *out);
@@ -74,5 +106,15 @@ err_t int_CastU64ToI64(uint64_t in, int64_t *out);
 err_t int_CastU64ToU8(uint64_t in, uint8_t *out);
 err_t int_CastU64ToU16(uint64_t in, uint16_t *out);
 err_t int_CastU64ToU32(uint64_t in, uint32_t *out);
+err_t int_CastU64ToSize(uint64_t in, size_t *out);
+
+err_t int_CastSizeToI8(size_t in, int8_t *out);
+err_t int_CastSizeToI16(size_t in, int16_t *out);
+err_t int_CastSizeToI32(size_t in, int32_t *out);
+err_t int_CastSizeToI64(size_t in, int64_t *out);
+err_t int_CastSizeToU8(size_t in, uint8_t *out);
+err_t int_CastSizeToU16(size_t in, uint16_t *out);
+err_t int_CastSizeToU32(size_t in, uint32_t *out);
+err_t int_CastSizeToU64(size_t in, uint64_t *out);
 
 #endif
